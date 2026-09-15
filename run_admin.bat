b56617520f7d640e22426220a8dc34d0306f4f1e
Muhammadali <batiyorovmuhammadali69@gmail.com>
2026-09-15 15:12:34 +0500

    first commit

---
+++ run_admin.bat
@@ -0,0 +1,8 @@
+@echo off
+title Kichik Alloma - Admin Panel (Standalone)
+echo ===================================================
+echo   KICHIK ALLOMA - ADMIN PANEL (STANDALONE)
+echo   Manzil: http://localhost:8080
+echo ===================================================
+python -m http.server 8080
+pause
